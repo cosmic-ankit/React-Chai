a67c69d184dabce15446f29a64d5154e3a60edd8
Ankit Jain <ankit9252984512@gmail.com>
2023-10-11 15:50:49 +0530

    Lecture 3- Making a component

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Chai from './Chai';
 
 function App() {
   return (
     <div className="App">
 <h1>Hello ankit</h1>
+<Chai/>
     </div>
   );
 }
---
+++ first-project/src/Chai.jsx
@@ -0,0 +1,9 @@
+function Chai ()
+{
+    return (<>
+    <h1>Hello this is Chai</h1>
+    </>)
+    
+}
+
+export default Chai
