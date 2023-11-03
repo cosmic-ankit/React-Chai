2e9296348df4a22632aab52fbcf8a2fab85e6343
Ankit Jain <ankit9252984512@gmail.com>
2023-11-03 15:14:40 +0530

    7.1 -> Understanding tailwind css

---
+++ 7-Props-and-tailwind/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+
+    // This added array will search in src and index.js
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
